163a3653cf38bb15c93c1ac2c9dc776dc81135d8
Raman1agrahari <115079147+Raman1agrahari@users.noreply.github.com>
2025-04-11 22:07:59 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
-
+import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import dotenv from "dotenv";
+import cookieParser from"cookie-parser"
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from"./routes/auth.route.js";
@@ -11,7 +11,9 @@ const app=express();
 
 const PORT=process.env.PORT;
 
-app.use("api/auth",authRoutes)
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth",authRoutes);
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,15 @@
+
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB =async ()=>{
     try{
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`mongoDB connected: ${connectDB.connection.host}`);
+         const conn=await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`mongoDB connected:${conn.connection.host}`);
     }
     catch(error){
         console.log("mongodb connection error:",error);
+        process.exit(0);
     }
 };
