1ff54438d676420a5cd2961b938839e1fe153c61
Raman1agrahari <115079147+Raman1agrahari@users.noreply.github.com>
2025-04-13 11:33:10 +0530

    feat

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary  from "../lib/cloudinary.js";
 
 export const signup= async(req,res)=>{
  
@@ -47,10 +48,68 @@ export const signup= async(req,res)=>{
    }
 };
 
-export const login=(req,res)=>{
-    res.send("login route");
+export const login=async(req,res)=>{
+     const {email,password} = req .body;
+     try{
+       const user = await User.findOne({email});
+       
+       if(!user){
+        return res.status(400).json({message:"Invalid crrredential"});
+       }
+
+       const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+       generateToken(user._id, res);
+
+       res.status(200).json({
+        _id: user._id,
+        fullName:user.fullName,
+        email:user.email,
+        profilePic:user.profilePic,
+       });
+     }catch(error){
+       console.log("Error in login controller",error.message);
+       res.status(500).json({ message: "Invalid Server Error"});
+     }  
   };
 
 export const logout=(req,res)=>{
-    res.send("logout route");
-  };
+  try{
+    res.cookie("jwt","",{ maxAge:0});
+    res.status(200).json({ message : "Logged out sucessfully"});
+
+  }catch(error){
+    console.log("Error in logout controller",error.message);
+       res.status(500).json({ message: "Invalid Server Error"});
+  }
+};
+
+export const updateProfile = async (req,res)=>{
+  try{
+    const {profilePic} =req.body;
+    const userId = req.user._id;
+
+    if(!profilePic){
+      return res.status(400).json({message:"Profile pic is required"});
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic)
+    const updateUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url}, {new:true})
+
+    res.status(200).json(updatedUser)
+
+  }catch(error){
+    console.log("Error in updated profile",error.message);
+    res.status(500).json({ message: "Internal Server Error"});
+  }
+};
+
+export const checkAuth = (req,res) => {
+  try{
+     res.status(200).json(req.user);
+
+  }catch(error){
+    console.log("Error in  checkAuth controller",error.message);
+    res.status(500).json({ message: "Internal Server Error"});
+  }
+}
---
+++ backend/src/lib/cloudinary.js
@@ -0,0 +1,13 @@
+import {v2 as cloudinary} from "cloudinary"
+
+import {config} from  "dotenv"
+
+config()
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.tAIwbjP3g2xQGD37pMQ55xJs1ec,
+});
+
+export default cloudinary;
