c348a3e05624ded4f19a0661330e7a5887eed962
Raman1agrahari <115079147+Raman1agrahari@users.noreply.github.com>
2025-08-17 02:21:18 +0530

    change

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,12 +3,12 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5003" : "/";
  
 export const useAuthStore = create((set, get)  => ({
    authUser: null,
    isSigningUp: false,
-   isLoggingIng: false,
+   isLoggingIn: false,
    isUpdatingProfile: false,
    isCheckingAuth: true,
    onlineUsers: [],
