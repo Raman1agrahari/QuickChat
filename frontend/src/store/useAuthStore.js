3528ec5d142b782f942d0c5df4e449a6ca7f477f
Raman1agrahari <115079147+Raman1agrahari@users.noreply.github.com>
2025-08-18 00:46:33 +0530

    change

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,8 +3,8 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5003" : "/";
- 
+const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_API}` : "/";
+
 export const useAuthStore = create((set, get)  => ({
    authUser: null,
    isSigningUp: false,
@@ -16,7 +16,7 @@ export const useAuthStore = create((set, get)  => ({
 
    checkAuth: async () => {
       try {
-         const res = await axiosInstance.get("/auth/check");
+         const res = await axiosInstance.get(`${import.meta.env.VITE_API}/auth/check`);
 
          set({ authUser: res.data });
          get().connectSocket();
@@ -32,7 +32,7 @@ export const useAuthStore = create((set, get)  => ({
    signup: async (data) => {
       set({ isSigningUp: true });
       try {
-         const res = await axiosInstance.post("auth/signup", data);
+         const res = await axiosInstance.post(`${import.meta.env.VITE_API}/auth/signup`, data);
           set({ authUser: res.data });
          toast.success("Account created successfully");
         get().connectSocket();
@@ -47,7 +47,7 @@ export const useAuthStore = create((set, get)  => ({
     login: async (data) => {
     set({ isLoggingIn: true });
     try {
-      const res = await axiosInstance.post("/auth/login", data);
+      const res = await axiosInstance.post(`${import.meta.env.VITE_API}/auth/login`, data);
       set({ authUser: res.data });
       toast.success("Logged in successfully");
       get().connectSocket();
@@ -60,7 +60,7 @@ export const useAuthStore = create((set, get)  => ({
 
      logout: async () => {
     try {
-      await axiosInstance.post("/auth/logout");
+      await axiosInstance.post(`${import.meta.env.VITE_API}/auth/logout`);
       set({ authUser: null });
       toast.success("Logged out successfully");
       get().disconnectSocket();
@@ -72,7 +72,7 @@ export const useAuthStore = create((set, get)  => ({
     updateProfile: async (data) => {
     set({ isUpdatingProfile: true });
     try {
-      const res = await axiosInstance.put("/auth/update-profile", data);
+      const res = await axiosInstance.put(`${import.meta.env.VITE_API}/auth/update-profile`, data);
       set({ authUser: res.data });
       toast.success("Profile updated successfully");
     } catch (error) {
