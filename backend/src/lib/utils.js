0c1abf908d0456b8dcf3b11cd14d1cffb884b588
Raman1agrahari <115079147+Raman1agrahari@users.noreply.github.com>
2025-08-31 15:46:59 +0530

    problem

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,16 +1,16 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
 
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    httpOnly: true,
-    sameSite: "none", // cross-site requests allow karne ke liye
-    secure: process.env.NODE_ENV === "production", // sirf production me https enforce
-  });
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        httpOnly: true,
+        sameSite: "none",
+        secure: process.env.NODE_ENV === "production",
+    });
 
-  return token;
+    return token;
 };
